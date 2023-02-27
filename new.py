95ddbaa217931427db4a318173c6e498849bb85e
Gajender <Iamsanju0707@gmail.com>
2023-02-27 10:01:38 +0530

    This is my first commit

---
+++ new.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.write("Hello everyone")
+
+st.markdown("""
+**this is bold**
+__this is for the italic__
+""",True
+)
+st.write("# hello",
+"## subham",
+"### rajat")
+
+d={"raja":92,"mohit":2,"subham":922,"kunal":92938}
+st.write(d)
